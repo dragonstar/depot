6ff72ff1969dc6474ba2aef519cee2b5ec60c32e
Charles Bacon <Charles@seitech.co.uk>
2011-12-08 21:35:09 +0000

    Initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,10 @@ class Product < ActiveRecord::Base
 validates :title, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01} 
 validates :title, :uniqueness => true
->	// validates :image_url, :allow_blank => true, :fomat => {
-> //   :with =>   %r{\.(gif|jpg|png)$}i,
-> //  :message => 'must be a url for gif, jpg or png image.'
->//}
+validates :image_url, :allow_blank => true, :format=> {
+    :with =>   %r{\.(gif|jpg|png)$}i,
+   :message => 'must be a url for gif, jpg or png image.'
+}
 
 
 end

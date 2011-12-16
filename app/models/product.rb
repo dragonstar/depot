ff8fb50bf5274f614d8564909841d19232490bef
Charles Bacon <charles@247Seas.com>
2011-12-16 00:41:11 +0000

    Merge to Master
    Conflicts:
    	app/views/products/index.html.erb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
+
+ has_many :line_items
+ before_destroy :ensure_not_referenced_by_any_line_items
+
+
+
 validates :title, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01} 
 validates :title, :uniqueness => true
@@ -6,6 +12,18 @@ validates :image_url, :allow_blank => true, :format=> {
     :with =>   %r{\.(gif|jpg|png)$}i,
    :message => 'must be a url for gif, jpg or png image.'
 }
+
+ private
+ # @return [Object]
+ def ensure_not_referenced_by_any_line_items
+   # code here
+   if line_items.empty?
+     return true
+   else
+     errors.add(:base, 'Line Items Present')
+     return false
+   end
+ end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111201223825) do
+ActiveRecord::Schema.define(:version => 20111215204239) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"

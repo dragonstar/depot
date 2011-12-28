2bbbcb6464fa320db407e2762c0015d025fef5f1
Charles Bacon <charles@247Seas.com>
2011-12-28 17:46:03 +0000

    moving cart to the sidebar - still CSS problems

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -82,6 +82,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to store_url }
+      format.js
       format.json { head :ok }
     end
   end
--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -10,4 +10,11 @@ class StoreControllerTest < ActionController::TestCase
 
     assert_select '.price', /&pound[,\d]+\.\d\d/
   end
+
+  test "markup needed for store.js.coffee is in place"  do
+    get :index
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]', 3
+  end
+
 end

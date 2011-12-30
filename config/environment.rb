da642d8a8b1fc8d35cb9ec4b07b25ea46016a644
Charles Bacon <charles@247Seas.com>
2011-12-30 15:17:01 +0000

    will_pagination plugin did not work - so this has been disabled for now atom, did not work - Then on the excercises - XTML, XML AND JSON did not work (though this is probably me! :)

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 
 
+#Rails::Initializer.run do |config|
+# config.gem "mislav-will_paginate"
+#end
+
+require 'will_paginate'
 # Initialize the rails application
 Depot::Application.initialize!
-require 'will_paginate'
+

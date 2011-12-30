eba0749c23ad41e6e56e29b3207a7d816519b5c9
Charles Bacon <charles@247Seas.com>
2011-12-30 16:36:26 +0000

    Starting email set-up

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,20 @@ require File.expand_path('../application', __FILE__)
 # config.gem "mislav-will_paginate"
 #end
 
+Depot::Application.configure do
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+     :address => "charles@seitech.co.uk",
+     :port => 110,
+     :domain => "domain.of.sender.net",
+     :authentication => "plain",
+     :user_name => "pt34460812-1",
+     :password => "shockwav",
+     :enable_starttls_auto => true
+}
+end
+
+
 require 'will_paginate'
 # Initialize the rails application
 Depot::Application.initialize!
